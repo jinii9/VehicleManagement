f914f23c171ef65dd03fabbeec2aaa8a523bc42f
vinivin153 <vinivin153@naver.com>
2024-12-06 14:09:28 +0900

    feat: 차량 데이터 클래스 구현

--- app/src/main/java/com/autoever/rightnow/models/Car.kt
+++ app/src/main/java/com/autoever/rightnow/models/Car.kt
@@ -1,5 +1,14 @@
 package com.autoever.rightnow.models
 
 data class Car(
-    val name: String = ""
+    var manufacturer: String = "",
+    var carType: String = "",
+    var carPerson: Int = 1,
+    var year: Int = 2024,
+    var image: String = "",
+    var carNumber: String = "",
+    var pricePerHour: Number = 0,
+    var carDescription: String = "",
+    var carLocationLat: String = "",
+    var carLocationLong: String = ""
 )
